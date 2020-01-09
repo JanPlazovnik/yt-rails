a675d620d45ab996f9fc06bb8fac3747c62f1488
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-01-10 00:13:42 +0100

    Add validation and user relation

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ApplicationRecord
     has_one_attached :clip
     has_one_attached :thumbnail
+    
+    validates :clip, presence: true, blob: { content_type: :video }
+    validates :thumbnail, blob: { content_type: :image }
+
+    belongs_to :user
 end
