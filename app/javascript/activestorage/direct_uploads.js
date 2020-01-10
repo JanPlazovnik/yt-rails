e29057f818733003a225dde881e69ef86b154cdb
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-01-10 23:28:40 +0100

    Add some event listeners to handle upload cancellation

--- app/javascript/activestorage/direct_uploads.js
+++ app/javascript/activestorage/direct_uploads.js
@@ -1,5 +1,12 @@
 window.addEventListener('turbolinks:load', () => {
-    if(document.getElementById("video_form")) {
+    if (document.getElementById("video_form")) {
+        
+        let xhr;
+        document.getElementById("video_clip").addEventListener("direct-upload:before-storage-request", (event) => xhr = event.detail.xhr);
+        document.addEventListener("turbolinks:request-start", (event) => {
+            if(xhr)
+                xhr.abort();             
+        })
 
         document.getElementById("video_clip").addEventListener("direct-upload:initialize", (event) => {
             const { target, detail } = event;
@@ -12,22 +19,22 @@ window.addEventListener('turbolinks:load', () => {
                 </div>
             `);
         })
-    
+
         document.getElementById("video_clip").addEventListener("direct-upload:start", (event) => {
             const { id } = event.detail;
             const element = document.getElementById(`direct-upload-${id}`);
             element.classList.remove("direct-upload--pending");
-            console.log("Started", id);;
-            
+            console.log("Started", id);
+
         });
-    
+
         document.getElementById("video_clip").addEventListener("direct-upload:progress", (event) => {
             const { id, progress } = event.detail;
             console.log(`Progress: ${progress}`);
             const progressElement = document.getElementById(`direct-upload-progress-${id}`);
             progressElement.style.width = `${progress}%`;
         });
-    
+
         document.getElementById("video_clip").addEventListener("direct-upload:error", (event) => {
             event.preventDefault();
             const { id, error } = event.detail;
@@ -35,13 +42,17 @@ window.addEventListener('turbolinks:load', () => {
             const element = document.getElementById(`direct-upload-${id}`)
             element.classList.add("direct-upload--error")
             element.setAttribute("title", error)
+            let abort_elem = document.getElementById("video_upload_abort");
+            abort_elem.parentNode.removeChild(abort_elem);
         });
-    
+
         document.getElementById("video_clip").addEventListener("direct-upload:end", (event) => {
             const { id } = event.detail;
             console.log("End", id);
             const element = document.getElementById(`direct-upload-${id}`)
             element.classList.add("direct-upload--complete")
+            let abort_elem = document.getElementById("video_upload_abort");
+            abort_elem.parentNode.removeChild(abort_elem);
         });
     }
 });
