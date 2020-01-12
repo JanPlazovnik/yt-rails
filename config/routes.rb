38e6a9b9e9cf28613b76617c6bdb8934e212474a
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-01-12 01:47:07 +0100

    Add routes for liking and disliking videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/', to: 'videos#index'
-  resources :videos
+  mount Commontator::Engine => '/commontator'
+  
+  resources :videos do
+    member do
+      put "like", to: "videos#upvote"
+      put "dislike", to: "videos#downvote"
+    end
+  end
+
+  root to: 'videos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
