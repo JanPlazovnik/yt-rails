6a1ded0547c9d6956799fcc2a345493855c9b661
JanPlazovnik <jan.plazovnik@gmail.com>
2020-01-15 23:04:44 +0100

    Remove follow redirect

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -10,12 +10,12 @@ class ProfileController < ApplicationController
 
     def follow
         current_user.follow!(@user)
-        redirect_back(fallback_location: root_path)
+        # redirect_back(fallback_location: root_path)
     end
 
     def unfollow
         current_user.unfollow!(@user)
-        redirect_back(fallback_location: root_path)
+        # redirect_back(fallback_location: root_path)
     end
 
     private
