db87c0f9239458ef08f349affd4413aab729a389
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-01-18 01:11:29 +0100

    Add a channel banner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :banner])
     end
 end
