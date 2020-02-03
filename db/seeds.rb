61ecedc4a1a2d529a3d9a818269ccb93e26caf8b
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-02-03 18:49:17 +0100

    Add a seed for prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'ytadmin@plazovnik.si', password: 'railstube', password_confirmation: 'railstube') if Rails.env.production?
